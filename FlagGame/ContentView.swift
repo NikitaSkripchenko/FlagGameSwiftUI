4932ebfd25d393a4b394fa6b995c0958d001fbec
Nikita <frickadelka456@gmail.com>
2019-06-18 21:12:08 +0300

    Initial Commit

---
+++ FlagGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FlagGame
+//
+//  Created by iosdev on 6/18/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
